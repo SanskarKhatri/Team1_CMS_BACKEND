923ff78207b7eae4d37383baebf79f92bae872d1
Sanskar Khatri <sanskarapkhatri@gmail.com>
2023-10-14 09:47:41 +0530

    Added Branch Location Repository and Controller.

--- src/main/java/com/ims/branchLocation/BranchLocation.java
+++ src/main/java/com/ims/branchLocation/BranchLocation.java
@@ -3,11 +3,20 @@ package com.ims.branchLocation;
 import com.ims.address.Address;
 import com.ims.company.Company;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
 public class BranchLocation {
 	
 	private long id;
+	
+	@NotEmpty(message = "{BranchLocation.name.NotEmpty}")
 	private String name;
+	
+	@NotNull(message = "{BranchLocation.company.NotNull}")
 	private Company company;
+	
+	@NotNull(message = "{BranchLocation.address.NotNull}")
 	private Address address;
 	
 	public long getId() {
